95834c3c974ff8ce93e39159980b0d9aec59aeeb
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-19 16:51:03 +0530

    user defined exception

---
+++ LambdaAndLinQ/AgeInvalidException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaAndLinQ
+{
+    internal class AgeInvalidException  : Exception
+    {
+        public string VMessage;
+        
+        public AgeInvalidException(string vmessage)
+        {
+            this.VMessage = vmessage;
+        }
+    }
+}
