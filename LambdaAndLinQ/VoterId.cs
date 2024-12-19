b3d726f0f2436a801d2c78c28f64c9d7ea780be0
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-19 16:51:23 +0530

    user defined exception

---
+++ LambdaAndLinQ/VoterId.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaAndLinQ
+{
+    internal class VoterId
+    {
+
+        public static void CheckVoter()
+        {
+            Console.WriteLine("enter your age");
+            int age=int.Parse(Console.ReadLine());
+
+            try
+            {
+                if(age<18)
+                {
+                    throw new AgeInvalidException("You are not eligible!!!");
+                }
+                else
+                {
+                    Console.WriteLine("You are eligible!!");
+                }
+            }
+            catch(AgeInvalidException e)
+            {
+                Console.WriteLine(e.VMessage);
+            }
+        }
+
+    }
+}
